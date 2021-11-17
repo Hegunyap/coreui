5ca166bbc6f8c030e5212eefbd14e43780dc6eca
Hendra Gunawan <hendra.yap@gmail.com>
2021-11-17 09:31:25 +0700

    Initiated coreui

---
+++ database/migrations/2021_11_14_102323_create_po_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('po', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('model');
+            $table->string('collection_name');
+            $table->string('name');
+            $table->string('file_name');
+            $table->string('mime_type')->nullable();
+            $table->string('disk');
+            $table->string('conversions_disk');
+            $table->unsignedBigInteger('size');
+            $table->unsignedBigInteger('uuid');
+            $table->json('manipulations');
+            $table->json('custom_properties');
+            $table->json('responsive_images');
+            $table->unsignedInteger('order_column')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('po');
+    }
+}
